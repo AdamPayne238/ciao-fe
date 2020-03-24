1226c97f0756337448bfdb7c215d6274b9895ebc
Adam Payne <ajpayne238@gmail.com>
2020-03-24 13:59:07 -0400

    removed websockets from index.js for FE deploy

--- ciao-app/src/index.js
+++ ciao-app/src/index.js
@@ -12,9 +12,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { setContext } from 'apollo-link-context'
 
 //Subscripions imports
-import { split } from 'apollo-link'
-import { WebSocketLink } from 'apollo-link-ws'
-import { getMainDefinition } from 'apollo-utilities'
+// import { split } from 'apollo-link'
+// import { WebSocketLink } from 'apollo-link-ws'
+// import { getMainDefinition } from 'apollo-utilities'
 
 
 const AUTH_TOKEN = process.env.AUTH_TOKEN
@@ -33,24 +33,26 @@ const authLink = setContext((_, {headers}) => {
   }
 })
 
-const wsLink = new WebSocketLink({
-  uri: `ws://ciao-be-vscode.herokuapp.com/`,
-  options: {
-      reconnect: true,
-      connectionParams: {
-          authToken: localStorage.getItem(AUTH_TOKEN),
-      }
-  }
-})
+// const wsLink = new WebSocketLink({
+//   uri: `ws://ciao-be-vscode.herokuapp.com/`,
+//   options: {
+//       reconnect: true,
+//       connectionParams: {
+//           authToken: localStorage.getItem(AUTH_TOKEN),
+//       }
+//   }
+// })
 
-const link = split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query)
-    return kind === 'OperationDefinition' && operation === 'subscription'
-  },
-  wsLink,
-  authLink.concat(httpLink)
-)
+// const link = split(
+//   ({ query }) => {
+//     const { kind, operation } = getMainDefinition(query)
+//     return kind === 'OperationDefinition' && operation === 'subscription'
+//   },
+//   wsLink,
+//   authLink.concat(httpLink)
+// )
+
+const link = authLink.concat(httpLink)
 
 const client = new ApolloClient({
   link,
