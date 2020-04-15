fd443ed71e181e666375b2a956504e4326870e78
Adam Payne <ajpayne238@gmail.com>
2020-04-15 13:19:53 -0400

    fixed flex box issue. 3 ciao app contianers are flexing correctly and generated messages for lack of data

--- ciao-app/src/components/app/sub/messages/message/Message.js
+++ ciao-app/src/components/app/sub/messages/message/Message.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Message.scss'
 
-const Messages = () => {
-
+const Message = () => {
+    var message = ["message1", "message2", "message3", "message4", "message5", "message6", "message7"]
     return(
-        <div className="message-container">
-          <p>Message Here</p>
-        </div>
+      <div className="message-container">
+        {message.map((messages) => <div className="message-stack"><p>{messages}</p></div>)}
+      </div>
     )
 }
 
-export default Messages
+export default Message
