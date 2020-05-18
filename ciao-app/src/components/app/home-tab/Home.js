4d8ab76012d2986991bc23075ee41c0eb8a09203
Adam Payne <ajpayne238@gmail.com>
2020-05-18 17:24:46 -0400

    home tab layout almost finished

--- ciao-app/src/components/app/home-tab/Home.js
+++ ciao-app/src/components/app/home-tab/Home.js
@@ -2,6 +2,12 @@ import React from 'react'
 import './Home.scss'
 import { useLocation } from 'react-router-dom'
 
+// SVG
+
+// SVG
+import Icon from '../../../global/Icon'
+import { ICONS } from '../../../global/IconConstants'
+
 
 const Home = () => {
 
@@ -28,16 +34,52 @@ const Home = () => {
                                 <p>Member Since: August, 2020</p>
                             </div>
 
-                        </div>
+                            <div className="ciao-home-underline"></div>
 
-                        <div className="ciao-home-bio">
-                            <h2>Bio</h2>
-                            <p>I am a full stack sofware Engineer with experience in mutliple languages and frameworks such as HTML, CSS, JavaScript, React, Node, Python, Apollo, and GraphQL. I am new to Ciao and looking to chat with other developers or catch up with friends. Ciao! </p>
-                        </div>
+                            <div className="ciao-home-bio">
+                                <h2>Bio</h2>
+                                <p>I am a full stack sofware Engineer with experience in mutliple languages and frameworks such as HTML, CSS, JavaScript, React, Node, Python, Apollo, and GraphQL. I am new to Ciao and looking to chat with other developers or catch up with friends. Ciao! </p>
+                            </div>
 
-                        <div className="ciao-home-status">
-                            <h2>Status</h2>
-                            <p>Busy coding by the beach, but not too busy to chat! </p>
+                            <div className="ciao-home-status">
+                                <h2>Status</h2>
+                                <p>Busy coding by the beach, but not too busy to chat! </p>
+                            </div>
+
+                            <div className="ciao-home-svg-contact">
+
+                                <div className="ciao-home-github">
+                                    <Icon
+                                        icon={ICONS.GITHUB}
+                                        width={32}
+                                        height={32}
+                                        color={pathname.includes('/home') ? '#E1473E' : '#EFC2C6'}
+                                    />
+                                    <p>GitHub</p>
+                                </div>
+
+                                <div className="ciao-home-linkedin">
+                                    <Icon
+                                        icon={ICONS.LINKEDIN}
+                                        width={32}
+                                        height={32}
+                                        color={pathname.includes('/home') ? '#E1473E' : '#EFC2C6'}
+                                    />
+                                    <p>LinkedIn</p>
+                                </div>
+
+                                <div className="ciao-home-email">
+                                    <Icon
+                                        icon={ICONS.EMAIL2}
+                                        width={32}
+                                        height={32}
+                                        color={pathname.includes('/home') ? '#E1473E' : '#EFC2C6'}
+                                    />
+                                    <p>Email</p>
+                                </div>
+
+                            </div>
+
                         </div>
                         
                     </div>
